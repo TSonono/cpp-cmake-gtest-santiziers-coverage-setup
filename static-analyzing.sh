d9fc85ed01388ace9a1b6e6a14efd40175f64c42
Tofik Sonono <47821555+TSonono@users.noreply.github.com>
2020-01-27 17:24:02 +0100

    Clang-tidy support
    Fixes and support for clang-tidy in static analysis

--- static-analyzing.sh
+++ static-analyzing.sh
@@ -4,14 +4,9 @@ REPORT_DIR="html"
 CMAKE_FLAGS=""
 
 SCAN_OPT="-enable-checker security.FloatLoopCounter -maxloop 100 \
-          -enable-checker core.DivideZero"
+          -enable-checker core.DivideZero"      
 
-# We can put additional checkers here for scan-build
-
-if [ ! -d "Debug" ]; then
-    mkdir Debug
-fi
-
+mkdir -p Debug
 cd Debug
 scan-build -v -o $REPORT_DIR $SCAN_OPT cmake $CMAKE_FLAGS ..
 make clean
@@ -20,3 +15,7 @@ echo ""
 
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 cppcheck --project=compile_commands.json --enable=warning --enable=style --enable=performance --enable=portability --enable=information
+
+echo ""
+cmake $CMAKE_FLAGS "-DCMAKE_C_CLANG_TIDY=clang-tidy;-checks=*" ..
+make -j4

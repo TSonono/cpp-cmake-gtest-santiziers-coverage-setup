63b3291045ff772862093706aef66f3c1e2b9097
Tofik Sonono <tofiksonono@msn.com>
2021-05-14 17:19:31 +0200

    Fixed static analysis

--- static-analyzing.sh
+++ static-analyzing.sh
@@ -1,21 +1,8 @@
-#!/bin/sh
-REPORT_DIR="html"
-
-CMAKE_FLAGS=""
-
-SCAN_OPT="-enable-checker security.FloatLoopCounter -maxloop 100 \
-          -enable-checker core.DivideZero"      
-
-mkdir -p Debug
-cd Debug
-scan-build -v -o $REPORT_DIR $SCAN_OPT cmake $CMAKE_FLAGS ..
-make clean
-scan-build -v -o $REPORT_DIR $SCAN_OPT make -j4
-echo ""
-
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
-cppcheck --project=compile_commands.json --enable=warning --enable=style --enable=performance --enable=portability --enable=information
+#!/bin/bash
+mkdir -p build && cd build && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .. && make -j && cd ..
 
 echo ""
-cmake $CMAKE_FLAGS "-DCMAKE_C_CLANG_TIDY=clang-tidy;-checks=*" ..
-make -j4
+cppcheck --project=build/compile_commands.json --addon=misra.py --enable=all --suppress=missingIncludeSystem --suppress=unusedFunction
+echo ""
+clang-tidy -p build src/*.cpp include/*.hpp
+echo ""

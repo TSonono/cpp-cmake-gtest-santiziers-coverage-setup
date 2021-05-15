9cc656f9cfc96fbcff5bbba13138f229ab875992
Tofik Sonono <tofiksonono@msn.com>
2021-05-15 12:14:56 +0200

    Added travis ci
    This includes running the unit tests, sending the coverage data to
    codecov.io and finally, running static analysis with cppcheck and
    clang-tidy.

--- static-analyzing.sh
+++ static-analyzing.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-mkdir -p build && cd build && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DSANITIZE_ADDRESS=Off -DBUILD_TESTS=Off -DWITH_COVERAGE=Off .. && ninja -j0 && cd ..
+mkdir -p build && cd build && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DSANITIZE_ADDRESS=Off -DBUILD_TESTS=Off -DWITH_COVERAGE=Off .. && make -j && cd ..
+
+set -e
 
 echo ""
-cppcheck --project=build/compile_commands.json --addon=misra.py --enable=all --suppress=missingIncludeSystem --suppress=unusedFunction
-echo ""
 clang-tidy -p build src/*.cpp include/*.hpp
+echo ""
+cppcheck --project=build/compile_commands.json --addon=misra.py --enable=all --error-exitcode=1 \
+--suppress=missingIncludeSystem --suppress=unusedFunction --suppress=misra-c2012-15.5 --suppress=unmatchedSuppression
 echo ""

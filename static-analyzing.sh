f10096baccce2de0d3122680cecdbbe84e9a7147
Tofik Sonono <tofiksonono@msn.com>
2021-05-15 12:00:43 +0200

    Minor changes

--- static-analyzing.sh
+++ static-analyzing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p build && cd build && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .. && make -j && cd ..
+mkdir -p build && cd build && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DSANITIZE_ADDRESS=Off -DBUILD_TESTS=Off -DWITH_COVERAGE=Off .. && ninja -j0 && cd ..
 
 echo ""
 cppcheck --project=build/compile_commands.json --addon=misra.py --enable=all --suppress=missingIncludeSystem --suppress=unusedFunction
